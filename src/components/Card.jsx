cb9bd225474c8a7afa36e07ce8fe282b77e2d012
Akshita Mathur <akshita.m127@gmail.com>
2024-10-22 11:03:38 +0530

    image changed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,8 +6,8 @@ function Card() {
     <div className='bg-[#023a15] w-[29em] h-[27em] absolute z-20 right-[14em] top-[27em] p-16'>
         <h2 className='text-white font-cormorant text-7xl'>Our Story</h2>
         <p className='text-white mt-8'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam accumsan arcu est, at lobortis nulla porta eu. Aenean vel dolor enim. Etiam aliquam mauris eros.</p>
-        <button className='mt-[3em] bg-white py-4 px-9 text-black rounded-full font-semibold text-sm flex items-center gap-2'><SlArrowRight />READ MORE</button>
-        <img className='w-[200px] ml-64 -mt-12' src='https://askproject.net/chloro/wp-content/uploads/sites/23/2021/08/Asset-2_5-1024x809.png'></img>
+        <button className='mt-[3em] bg-white py-4 px-9 text-black rounded-full font-semibold text-sm lex items-center gap-2'><SlArrowRight />READ MORE</button>
+        <img className='w-[200px] ml-64 -mt-12' src='https://askproject.net/chloro/wp-content/uploads/sites/23/2f021/08/Asset-2_5-1024x809.png'></img>
     </div>
   )
 }
